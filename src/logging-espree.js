829e861049b839c72494e895f0464f1fd2f993a7
Marta Molina Fernández <83011996+MartaMolina01@users.noreply.github.com>
2023-02-28 18:17:20 +0000

    funciona - tests

--- src/logging-espree.js
+++ src/logging-espree.js
@@ -4,13 +4,31 @@ import * as estraverse from "estraverse";
 import * as fs from "fs/promises";
 
 export async function transpile(inputFile, outputFile) {
-  // Fill in the code here
+  let input = await fs.readFile(inputFile, 'utf-8')
+  const output = addLogging(input);
+  if (outputFile === undefined) {
+      console.log(output);
+      return;
+  }
+  await fs.writeFile(outputFile, output)
 }
 
 export function addLogging(code) {
-  // Fill in the code here
+  const ast = espree.parse(code, { ecmaVersion: espree.latestEcmaVersion, loc: true});
+  estraverse.traverse(ast, {
+    enter: function(node, parent){
+      if (node.type === 'FunctionDeclaration' || node.type === 'FunctionExpression') {
+        addBeforeCode(node);
+      }
+    }
+  });
+  return escodegen.generate(ast);
 }
 
 function addBeforeCode(node) {
- // Fill in the code here
+  const name = node.id ? node.id.name : '<anonymous function>' ;
+  const args = node.params.map(p => `$\{${p.name}}`).join(', ');
+  const beforeCode = "console.log(`Entering " + name + "(" + args + ") at line " + node.loc.start.line + "`);";
+  const beforeNodes = espree.parse(beforeCode, { ecmaVersion: espree.latestEcmaVersion, loc: true }).body;
+  node.body.body = beforeNodes.concat(node.body.body);
 }
